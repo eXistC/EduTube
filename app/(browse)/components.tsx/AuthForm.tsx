f920c2840c3dd3d3e7e00514f9359482aa456ad4
boss10801 <boss101801@gmail.com>
2024-04-09 14:55:15 +0700

    Routeable now Register still a problem

--- app/(browse)/components.tsx/AuthForm.tsx
+++ app/(browse)/components.tsx/AuthForm.tsx
@@ -4,6 +4,7 @@ import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
 import axios from "axios";
 import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
@@ -12,14 +13,15 @@ type Variant = 'LOGIN' | 'REGISTER'
 
 const AuthForm = () => {
     const session = useSession();
+    const router = useRouter();
     const [variant, setVariant] = useState<Variant>('LOGIN');
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         if (session?.status === 'authenticated'){
-            console.log('Authenticated')
+            router.push('/home')
         }
-    }, [session?.status]);
+    }, [session?.status ,router]);
 
     const toggleVariant = useCallback(() => {
         if (variant === 'LOGIN'){
