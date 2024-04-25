2b32e6b31af545a215085a8c93b67937b0c4ba85
boss10801 <boss101801@gmail.com>
2024-04-25 13:22:32 +0700

    Make Singout button Useable

---
+++ hooks/useRoutes.ts
@@ -0,0 +1,18 @@
+import { useMemo } from "react";
+import { usePathname } from "next/navigation";
+import { signOut } from "next-auth/react";
+
+const useRoutes = () => {
+  const pathname = usePathname();
+  const routes = useMemo(() => [
+    {
+      label: 'Logout',
+      href: '/',
+      onClick: () => signOut(),
+    }
+  ], [pathname]);
+
+  return routes;
+}
+
+export default useRoutes;
