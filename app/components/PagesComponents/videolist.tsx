501e328c3b9295450b7e6b2b2714445b21d40869
eXistC <exist.hpls@gmail.com>
2024-04-29 14:39:37 +0700

    search result ui framework 	modified:   app/components/PagesComponents/videolist.tsx 	modified:   app/home/page.tsx

--- app/components/PagesComponents/videolist.tsx
+++ app/components/PagesComponents/videolist.tsx
@@ -14,13 +14,15 @@ const VideoList: React.FC<VideoListProps> = ({ data, title, children }) => {
     }
 
     return (
-        <div className="px-4 md:px-12 mt-4 space-y-8">
-            <h2 className="text-2xl font-semibold text-neutral-100">
+        <div className="px-4 md:px-64 mt-4 space-y-8">
+            <h2 className="text-lg font-inter font-semibold text-neutral-100/30">
                 {title}
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-
+                <div className="bg-grey-100"></div>
             </div>
         </div>
     );
 }
+
+export default VideoList;
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import useRoutes from '@/app/hooks/useRoutes';
 import SearchBar from '../components/PagesComponents/searchbar';
+import VideoList from '../components/PagesComponents/videolist';
 
 const HomePage = () => {
   const routes = useRoutes();
@@ -9,10 +10,14 @@ const HomePage = () => {
   return(
     
     <div className="min-h-screen bg-radial-gradient flex flex-col justify-center sm:py-12">
-      <h1 className="text-4xl font-bold font-inter text-center text-white mb-8 -mt-32 whitespace-normal">What would you like to learn today?</h1>
+      <h1 className="text-4xl font-bold font-inter text-center text-white mb-16 -mt-64 whitespace-normal">What would you like to learn today?</h1>
       <SearchBar />
-    
-  </div>
+      <div className= "px-80 pt-16">
+      <VideoList data={["test"]} title="SEARCH RESULTS">
+        <div className="bg-grey-100"></div>
+      </VideoList> 
+      </div>
+    </div>
   );
 };
 
