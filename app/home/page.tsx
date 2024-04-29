71e7228e618af36e29fa628c0d5422a0bf74b606
eXistC <exist.hpls@gmail.com>
2024-04-29 11:56:16 +0700

    oppsie fixes

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,13 @@
 'use client';
 import React from 'react';
-import Button from "@/app/components/Button";
 import useRoutes from '@/app/hooks/useRoutes';
-import searchbar from '../components/PagesComponents/searchbar';
 import SearchBar from '../components/PagesComponents/searchbar';
 
 const HomePage = () => {
   const routes = useRoutes();
 
   return(
+    
     <div className="min-h-screen bg-radial-gradient flex flex-col justify-center sm:py-12">
       <h1 className="text-4xl font-bold font-inter text-center text-white mb-8 -mt-32 whitespace-normal">What would you like to learn today?</h1>
       <SearchBar />
