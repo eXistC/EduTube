9d3ecd5cfbf6571356e0225d68ecc11fe3aca284
eXistC <pongpanod7pu@gmail.com>
2024-05-02 13:08:30 +0700

    minor stylize on the videoplayer close button

--- app/components/PagesComponents/videoplayer.tsx
+++ app/components/PagesComponents/videoplayer.tsx
@@ -14,7 +14,7 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoUrl, onClose }) => {
     return ReactDOM.createPortal(
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
             <div className="relative w-4/5 h-4/5 rounded-lg overflow-hidden">
-                <button onClick={onClose} className="absolute top-5 right-5 bg-white rounded-full p-2 cursor-pointer">X</button>
+                <button onClick={onClose} className="absolute top-5 right-5 bg-teal-300 rounded-full p-2 cursor-pointer font-inter font-bold py-0">X</button>
                 <iframe src={videoUrl} className="w-full h-full" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
             </div>
         </div>,
