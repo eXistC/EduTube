f6c33a9f8876943bacb845d6e749a0ecbf70bcbe
SKBOYYNW <belong.timen@gmail.com>
2024-05-07 22:32:00 +0700

    git up7/5

--- app/(auth)/(browse)/(home)/page.tsx
+++ app/(auth)/(browse)/(home)/page.tsx
@@ -1,7 +1,7 @@
 
 function App() {
     return (
-        <div className="App">
+        <div className="App bg-emerald-700 h-screen">
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
@@ -9,7 +9,9 @@ function App() {
                     </a>
                     <div className="relative inline-block text-left">
                         <div>
-                            <button type="button" className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                            <button type="button" className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 
+                            text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" 
+                            aria-expanded="true" aria-haspopup="true">
                                 Options
                                 <svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
@@ -29,10 +31,10 @@ function App() {
                     </div>
                 </div>
             </nav>
-            <div className="bg-emerald-700 h-screen flex items-center justify-center">
+
+            <div className="flex items-center justify-center pt-100">
                 <form className="max-w-md mx-auto">
                     <label className='self-center text-3xl font-semibold whitespace-nowrap dark:text-white'>What are you interested to learn?</label> 
-
                     <div className="relative">
                         <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -43,6 +45,10 @@ function App() {
                         <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                     </div>
                 </form>
+                
+            </div>
+            <div className="mt-8 mx-10">
+                    <h2 className="text-xl mb-4">Continue learning</h2>
             </div>
         </div>
     );
